4f42172b70eafa3876050c83429cdbd88b4dc01a
SeverinoArmando <severinoarmando3@gmail.com>
2021-09-22 15:12:57 -0300

    Atualizando

--- index.js
+++ index.js
@@ -16,16 +16,16 @@ app.use(express.json());
 const routes = require('./routes/routes')
 routes(app);
 
-mongoose.connect('mongodb://localhost:27017/healthFood', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/healthFood', { useNewUrlParser: true })
 
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Falha ao se conectar ao Banco de Dados: '));
+db.on('error', console.error.bind(console, 'Falha ao se conectar ao Banco de Dados: ' ));
 
 db.once('open', function () {
     console.log('Conectado o com sucesso ao Banco de Dados')
 
     app.listen(app.get('port'), function () {
-        console.log(`App rodando na url http:localhost:${app.get('port')}`)
+        console.log(`App rodando na url http://localhost:${app.get('port')}`)
     })
 })
